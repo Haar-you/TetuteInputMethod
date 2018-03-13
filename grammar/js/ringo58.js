1973f5357195618c3ca78440fe7137171df7bcf2
Haar-you <zemlya.pole@gmail.com>
2018-03-13 17:43:17 +0900

    りんご58文のページ改訂

--- grammar/js/ringo58.js
+++ grammar/js/ringo58.js
@@ -1,13 +1,3 @@
 window.addEventListener("load", function(){
-    //ringo58文にidと番号割り当て
-    var ringo58 = document.getElementsByClassName("ringo58");
-
-
-    Array.prototype.forEach.call(ringo58, function(elem, index){
-	var number = document.createElement("div");
-	number.innerHTML = `${index+1}`;
-	
-	elem.id = `ringo58-${index+1}`;
-	elem.insertBefore(number, elem.firstChild);
-    });
+    makeTetuteGloss("./json/ringo58.json", "ringo", document.body, false);
 });
